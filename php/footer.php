9299c0a9aada67c8d72e8ad0d2a49123f7d53122
Kunal Raut <rautkunal00@gmail.com>
2020-03-10 20:02:22 +0530

    update main page

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer ">
   <div class="mx-4">
-    <span class="text-muted float-left">Copyright <i class="far fa-copyright"></i> 2020 All rights reserved.</span>
-    <span class="text-muted float-right">aakrut@gmail.com</span>
+    <span class="text-muted float-left">Copyright <i class="far fa-copyright"></i> 2020 by aakrut.com All rights reserved.</span>
+    <span class="text-muted float-right">contact@aakrut.com</span>
   </div>
 </footer>
 
