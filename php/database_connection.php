911acfb8294c8a80a842caf3c5ac2b1bd0f369d4
Kunal Raut <rautkunal00@gmail.com>
2021-03-29 20:56:55 +0530

    Deplayment properties do not modify

--- php/database_connection.php
+++ php/database_connection.php
@@ -1,7 +1,7 @@
 <?php 
-
+  error_reporting(0);
   //database_connection.php
   $limit = 4;
-  $connect = new PDO("mysql:host=localhost;dbname=db_aakrut", "root", "");
+  $connect = new PDO("mysql:host=localhost;dbname=u648693752_aakrut", "u648693752_aakrut", "u648693752_aakrut");
 
 ?> 
